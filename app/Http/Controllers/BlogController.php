9d4e50594e05a003a345921a5f2b2b50a42838fc
Kajalmondal1 <kajalmondal148@gmail.com>
2024-02-17 21:04:44 +0530

    Blogs added

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    public function createBlog(Request $request){
+        $validator = Validator::make($request->all(), [
+            'Title' => 'string ',
+            'content' => 'required | string',
+            'img' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["status" => false, "message" => $validator->errors()], 422);
+        }
+        else{
+            $imgLink= "https://php-file-upload-learning.s3.ap-southeast-2.amazonaws.com/Mmagic/Kajal-Mondal1708086341.png";
+            $validatedPostData=[
+                'creator_id'=>Auth::user()->id,
+                'Title'=>$request->title,
+                'img'=>$imgLink,
+                'content'=>$request->content,
+            ];
+            $blog=Blog::create($validatedPostData);
+            if($blog){
+                return response()->json(["status" => true, "message" => 'Vlog Created'], 201);
+            }
+            else{
+                return response()->json(["status" => false, "message" => 'Failed to create the blog'], 422);
+            }
+        }
+    }
+    public function getAllBlog(){
+        $id=Auth::user()->id;
+        $allBlog=Blog::find($id);
+        if($allBlog){
+            return response()->json(["status"=>true,"messsage"=>"Blogs Returned Successfully","blogs"=>$allBlog,"Total-blog"=>$allBlog->count()]);
+        }
+        else{
+            return response()->json(["status"=>false,"messsage"=>"Failed to returns the blogs"]);
+        }
+    }
+    public function deleteBlog($BlogId){
+        $user_id=Auth::user()->id;
+        $blog=Blog::where('id',$BlogId)->where('creator_id',$user_id);
+        if($blog){
+            Blog::destroy($BlogId);
+            return response()->json(["staus"=>true,"message"=>"Blog Deleted Successfully"],200);
+        }
+        else{
+            return response()->json(["staus"=>false,"message"=>"You can't Delete this post"],422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 Route::get('profile',[AuthController::class,'getDetails'])->middleware('jwtVerify');
+Route::post('createblog',[BlogController::class,'createBlog'])->middleware('jwtVerify');
+Route::get('get-all-blog',[BlogController::class,'getAllBlog'])->middleware('jwtVerify');
+Route::delete('delete-blog/{BlogID}',[BlogController::class,'deleteBlog'])->middleware('jwtVerify');
